5cda75ef6de4d50480ea672af693af6238632d33
Darren Zhu <joodongri@gmail.com>
2019-11-05 16:27:42 -0800

    search movies, get movie detail, and getting now playing movies work at back end, added relevant functions at front end

--- server/server.js
+++ server/server.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cookieParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const path = require("path")
 
 const PORT = 3000;
 const app = express();
 
+const movieRouter = require('./routes/movie');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-   res.status(200).sendFile(path.resolve(__dirname, '../src/views/index.html'));
+app.use('/movie', movieRouter);
+ 
+app.get("/",  (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, '../src/views/index.html'));
+});
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => {
+  return res.status(404).send('Page Not Found');
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' }, 
+  };
+
+  let errObj = Object.assign(defaultErr, err);
+  return res.status(errObj.status).send(errObj.message);
 })
 
 app.listen(PORT, () => {
