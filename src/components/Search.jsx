c5c5c79532a15add31d697dfff9e5b7ea9577665
brad morgan <bkmorgan3@gmail.com>
2019-11-06 18:27:36 -0800

    edits search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 
 export default function Search (props) {
   return (
-    <form className="search-form" onSubmit={props.getSearchResult}>
-      <input type='text' name='searched' placeholder='Find Movies' className='input-field' />
-      <button type='submit'>Search</button>
-    </form>
+    <div className="login">
+      <h1>Search for a Movie</h1>
+      <form className="search-form" onSubmit={props.getSearchResult}>
+        <input type='text' name='searched' placeholder='Find Movies' className='input-field' />
+        <button type='submit'>Search</button>
+      </form>
+    </div>
   )
 }
